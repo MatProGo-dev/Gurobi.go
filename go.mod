efb4dd2cb8e3566d1919251f6fcd9f26759d49e2
Kwesi Rutledge <kwesir@mit.edu>
2023-03-19 21:22:33 -0400

    Moved Some Tests

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MatProGo-dev/Gurobi.go
 
 go 1.19
+
+require github.com/MatProGo-dev/MatProInterface.go v0.0.0-20230311020709-c18dd030e857 // indirect
---
+++ testing/mpgSolver/qp_test.go
@@ -0,0 +1,16 @@
+package mpgSolver_test
+
+import "testing"
+
+/*
+TestQP1
+Description:
+
+	Creates a simple QP that we wish to minimize using the tools of Gurobi.
+*/
+func TestQP1(t *testing.T) {
+	// Constants
+
+	// Modeling
+
+}
